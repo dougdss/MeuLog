b6103ec71ac2053cb15924630f8bd89e30a78c9b
dougdss <douglas.sstt@gmail.com>
2024-10-11 00:59:15 -0300

    first commit

---
+++ Sources/MeuLog/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  MeuLog
+//
+//  Created by Douglas Santos on 11/10/24.
+//
+
+import Foundation
+
+public class Logger {
+    public static func log(message: String) {
+        print("[LOGGER SPM]: \(message)")
+    }
+}
